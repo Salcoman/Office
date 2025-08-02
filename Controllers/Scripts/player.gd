eccbe9056cf0ede9bdc6e4fe90341c70afc0fa3d
Branko Sulovic <44022084+Salcoman@users.noreply.github.com>
2025-08-02 05:39:22 +0200

    rework camera, add viewport, add interactibe UI to viewport, add viewport to mesh

--- Controllers/Scripts/player.gd
+++ Controllers/Scripts/player.gd
@@ -6,17 +6,24 @@ extends CharacterBody3D
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
 @export var CAMERA_CONTROLLER : Camera3D
 
+@onready var cam = $Camera3D
 
 var _mouse_input: bool = false
-var _mouse_rotation : Vector3
+var _mouse_rotation : Vector3 
 var _rotation_input: float
 var _tilt_input : float
-var _player_rotation : Vector3
-var _camera_rotation : Vector3
+var _player_rotation : Vector3 
+var _camera_rotation : Vector3 
+var mouse = Vector2()
+var HIGH_MOUSE_SPEED = 10
+var LOW_MOUSE_SPEED = 2
+var current_mouse_speed = HIGH_MOUSE_SPEED
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
+	if event is InputEventMouseMotion:
+		mouse = event.relative
 
 func _unhandled_input(event: InputEvent) -> void:
 	_mouse_input = event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED
@@ -30,7 +37,7 @@ const JUMP_VELOCITY = 4.5
 
 
 func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 
 func _update_camera(delta) -> void:
 	_mouse_rotation.x += _tilt_input * delta
@@ -49,10 +56,24 @@ func _update_camera(delta) -> void:
 	_tilt_input = 0.0
 	
 func _physics_process(delta: float) -> void:
-	_update_camera(delta)
+	
+	if Input.is_action_just_pressed("switch_mouse"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			current_mouse_speed = LOW_MOUSE_SPEED
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+			current_mouse_speed = HIGH_MOUSE_SPEED
+			
+	#If mouse visible return(prevent movement)
+	if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+		return
+	
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-
+	rotation_degrees.y -= mouse.x * current_mouse_speed * delta
+	cam.rotation_degrees.x -= mouse.y * current_mouse_speed * delta
+	cam.rotation_degrees.x = clamp(cam.rotation_degrees.x, -80,80)
 	if direction != Vector3.ZERO:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
@@ -64,4 +85,6 @@ func _physics_process(delta: float) -> void:
 		velocity.y = JUMP_VELOCITY
 
 	velocity.y -= 9.8 * delta  # gravity
+	#_update_camera(delta)
 	move_and_slide()
+	mouse = Vector2()
