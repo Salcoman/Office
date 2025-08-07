9400658beaf5db89f45dac8b7043a763b3148150
Stari-Stari <stojimirovic.djordje@gmail.com>
2025-08-07 18:38:24 +0200

    add manager info to docs

--- Controllers/Scripts/docs.gd
+++ Controllers/Scripts/docs.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var intro : VBoxContainer = $HBoxContainer/IntroContainer
 @onready var sequence : ScrollContainer = $HBoxContainer/SequenceUIContainer
+@onready var manager : ScrollContainer = $HBoxContainer/ManagementContainer
 
 
 func _on_docs_list_item_selected(index: int) -> void:
@@ -12,8 +13,12 @@ func _on_docs_list_item_selected(index: int) -> void:
 		1:
 			hide_all_containers()
 			sequence.set_deferred("visible", true)
+		2:
+			hide_all_containers()
+			manager.set_deferred("visible", true)
 
 
 func hide_all_containers():
 	intro.set_deferred("visible", false)
 	sequence.set_deferred("visible", false)
+	manager.set_deferred("visible", false)
